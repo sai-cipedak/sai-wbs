ea9ad82a66d5d86b115887f13e1fedbf56c421c2
DS Cipedak <rahmahakim@yahoo.co.id>
2026-08-28 20:24:49 +0700

    security: restrict UAT link propagation trigger function

---
+++ supabase/migrations/20260828132436_lock_uat_link_propagation_function.sql
@@ -0,0 +1,3 @@
+revoke all on function public.propagate_test_flag_to_linked_case() from public;
+revoke all on function public.propagate_test_flag_to_linked_case() from anon;
+revoke all on function public.propagate_test_flag_to_linked_case() from authenticated;
